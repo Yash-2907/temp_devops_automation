6baa0219e5ac6fc9c994d6e1229506f751705482
Yash-2907 <guptayp_1@rknec.edu>
2025-04-20 19:44:54 +0530

    project intialized

---
+++ src/test/java/com/example/temp_devops_automation/TempDevopsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temp_devops_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempDevopsAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
